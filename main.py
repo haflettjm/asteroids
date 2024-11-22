2622afd28ca5859ffe0e631d01bdbb10ead86131
Jacob Haflett <hafletjm01@gmail.com>
2024-11-22 10:38:03 -0500

    added some framerate limiting!

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from constants import *
 
-def game_loop():
+def game_loop(gameClock, dt):
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -13,12 +13,16 @@ def game_loop():
         screen_surface = pygame.display.get_surface()
         screen_surface.fill(black)
         pygame.display.flip()
+        framerate = 60
+        dt = (gameClock.tick(framerate))/1000
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    game_loop()
+    gameClock = pygame.time.Clock()
+    dt = 0
+    game_loop(gameClock, dt)
 
 if __name__ == "__main__":
     main()
