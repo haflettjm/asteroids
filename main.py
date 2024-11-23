dab68ee5c599ce9313897c45fce34ea99ed09e99
Jacob Haflett <hafletjm01@gmail.com>
2024-11-23 08:35:12 -0500

    yay player has been added wooooo

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import pygame
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from constants import *
+from player import Player
 
-def game_loop(gameClock, dt):
+def game_loop(gameClock, dt, PlayerX):
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -12,6 +13,7 @@ def game_loop(gameClock, dt):
         black = (0, 0, 0)
         screen_surface = pygame.display.get_surface()
         screen_surface.fill(black)
+        PlayerX.draw(screen)
         pygame.display.flip()
         framerate = 60
         dt = (gameClock.tick(framerate))/1000
@@ -22,7 +24,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     gameClock = pygame.time.Clock()
     dt = 0
-    game_loop(gameClock, dt)
+    PlayerX = Player(SCREEN_WIDTH /2 , SCREEN_HEIGHT / 2)
+    game_loop(gameClock, dt, PlayerX)
 
 if __name__ == "__main__":
     main()
